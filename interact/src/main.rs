ad71668030300d95b2b33621a699c20c56c2042d
Reto Trinkler <reto.trinkler@gmail.com>
2016-07-14 17:59:06 +0200

    output

--- interact/src/main.rs
+++ interact/src/main.rs
@@ -8,15 +8,21 @@ use curl::easy::Easy;
 // Print a web page onto stdout
 fn main() {
     let mut data = "{\"jsonrpc\":\"2.0\",\"method\":\"web3_clientVersion\",\"params\":[],\"id\":1}".as_bytes();
-
+    let mut resp = Vec::new();
     let mut easy = Easy::new();
     easy.url("http://localhost:8545").unwrap();
     easy.post(true).unwrap();
     easy.post_field_size(data.len() as u64).unwrap();
 
-    let mut transfer = easy.transfer();
-    transfer.read_function(|buf| {
-        Ok(data.read(buf).unwrap_or(0))
-    }).unwrap();
-    transfer.perform().unwrap();
+    {
+        let mut transfer = easy.transfer();
+        transfer.read_function(|buf| {
+            resp.extend_from_slice(buf);
+            Ok(data.read(buf).unwrap_or(0))
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
+
+    println!("{:?}", easy.response_code().unwrap());
+    println!("{:?}", resp);
 }
